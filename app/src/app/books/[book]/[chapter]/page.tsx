5154a64df63092d45e5dbe66929ac51b72085373
flyinglimao <hi@limaois.me>
2025-04-25 19:28:29 +0900

    chore: deploy vercel

--- app/src/app/books/[book]/[chapter]/page.tsx
+++ app/src/app/books/[book]/[chapter]/page.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 export default function Chapter() {
   return <div></div>;
 }
--- app/src/app/desk/[book]/page.tsx
+++ app/src/app/desk/[book]/page.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 export default function Book() {
   return <div></div>;
 }
